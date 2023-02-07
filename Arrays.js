3d52296aa6c89652c1505611bff8c6e3c2d40978
IrinaChizhova <iriska.verbnaya@gmail.com>
2023-02-08 00:04:50 +0300

    Added file Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,9 @@
+let balance = 500;
+let codeFromSms = 'A001DFX0';
+const postamat = [null, null, null, 'A001DFX0', null];
+
+if ((indexCell = codeFromSms)) {
+  balance = balance - 100;
+  let indexCell = postamat.indexOf('A001DFX0') + 1;
+  console.log(`Parcel received from ${indexCell}, balance ${balance}`);
+}
