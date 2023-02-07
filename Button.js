b7e96c277dc5b5fd185e7a0e17e94819225c9398
IrinaChizhova <iriska.verbnaya@gmail.com>
2023-02-08 00:12:15 +0300

    Added file Button.js

---
+++ Button.js
@@ -0,0 +1,15 @@
+class Button {
+  constructor(width, length, type, color) {
+    this.width = width;
+    this.length = length;
+    this.type = type;
+    this.color = color;
+  }
+}
+let myNewOneClick = new Button(5, 4, 'button', 'green');
+console.log(myNewOneClick);
+
+function testButton() {
+  !myNewOneClick.hasOwnProperty(width);
+}
+console.log(myNewOneClick.width);
